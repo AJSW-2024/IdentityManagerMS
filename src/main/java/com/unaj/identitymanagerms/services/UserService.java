7af656aebf3efea9b923db4a2317e69509a61970
DESKTOP-74767HC\leomo <leann1495@gmail.com>
2024-10-25 19:38:37 -0300

    agrego service

--- src/main/java/com/unaj/identitymanagerms/services/UserService.java
+++ src/main/java/com/unaj/identitymanagerms/services/UserService.java
@@ -1,47 +1,102 @@
 package com.unaj.identitymanagerms.services;
 
+import com.unaj.identitymanagerms.entities.Role;
 import com.unaj.identitymanagerms.entities.User;
+import com.unaj.identitymanagerms.repository.RoleRepository;
+import com.unaj.identitymanagerms.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements IUserService, UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public Optional<User> findById(Long id) {
-        return Optional.empty();
+        return userRepository.findById(id);
     }
 
     @Override
     public Optional<User> findByUsername(String username) {
-        return Optional.empty();
+        return userRepository.findByUsername(username);
     }
 
     @Override
     public Iterable<User> findAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public User save(User user) {
-        return null;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        user.setRoles(getRoles(user));
+        user.setEnabled(true);
+        return userRepository.save(user);
     }
 
     @Override
     public Optional<User> update(User user, Long id) {
-        return Optional.empty();
+        Optional<User> userOptional = this.findById(id);
+
+        return userOptional.map(userDb -> {
+            userDb.setEmail(user.getEmail());
+            userDb.setUsername(user.getUsername());
+            if (user.isEnabled() == null) {
+                userDb.setEnabled(true);
+            } else {
+                userDb.setEnabled(user.isEnabled());
+            }
+            userDb.setRoles(getRoles(user));
+
+            return Optional.of(userRepository.save(userDb));
+        }).orElseGet(() -> Optional.empty());
     }
 
     @Override
     public void delete(Long id) {
-
+        userRepository.deleteById(id);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user= userRepository.findByUsername(username).orElseGet(User::new);
+        List<GrantedAuthority> roles = user.getRoles().stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toList());
+            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.isEnabled(), true, true, true, roles);
+    }
+
+    private List<Role> getRoles(User user) {
+        List<Role> roles = new ArrayList<>();
+        Optional<Role> roleOptional = roleRepository.findByName("ROLE_USER");
+        roleOptional.ifPresent(roles::add);
+
+        if (user.isAdmin()) {
+            Optional<Role> adminRoleOptional = roleRepository.findByName("ROLE_ADMIN");
+            adminRoleOptional.ifPresent(roles::add);
+        }
+        return roles;
     }
 }
+
+
